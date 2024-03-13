f0244a627c2ec3bacc2dcbd068ebc67ed6944759
RajPasupuleti <161283756+RajPasupuleti@users.noreply.github.com>
2024-03-13 21:22:10 +0530

    Delete DogBiteRegistry directory

--- DogBiteRegistry/src/main/java/DBRMain.java
+++
@@ -1,9 +0,0 @@
-import controller.VictimController;
-
-public class DBRMain {
-
-    public static void main(String[] args) {
-        VictimController controller = new VictimController();
-        controller.start();
-    }
-}
